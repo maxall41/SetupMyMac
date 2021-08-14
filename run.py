e3b831442ca25e07f540f4a90203709cc4e6484d
max <maxall4@protonmail.com>
2021-08-14 13:38:51 -0700

    removed no-redis

--- run.py
+++ run.py
@@ -118,20 +118,20 @@ for file_name in file_names:
     shutil.move(os.path.join(source_dir, file_name), target_dir)
 
 # Install APPS
-    
-import os
-import shutil
 
-try:
-    os.system("git clone git@github.com:maxall41/apps-no-redis.git")
-except:
-    print("")
+# import os
+# import shutil
 
-source_dir = './apps-no-redis'
-target_dir = '~/Applications'
-    
-file_names = os.listdir(source_dir)
-    
-for file_name in file_names:
-    if (file_name[0] != "."):
-        shutil.unpack_archive(os.path.join(source_dir, file_name), target_dir)
+# try:
+#     os.system("git clone git@github.com:maxall41/apps-no-redis.git")
+# except:
+#     print("")
+
+# source_dir = './apps-no-redis'
+# target_dir = 'C:/Users/maxcampbell/'
+
+# file_names = os.listdir(source_dir)
+
+# for file_name in file_names:
+#     if (file_name[0] != "."):
+#         shutil.unpack_archive(os.path.join(source_dir, file_name), target_dir)
