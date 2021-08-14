fe9f43aa0060802398a084ebf1b9b228db5e869a
max <maxall4@protonmail.com>
2021-08-14 08:45:21 -0700

    first commit

---
+++ run.py
@@ -0,0 +1,75 @@
+import os
+
+
+# Install homebrew
+
+os.system("/bin/bash -c '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)'")
+
+# Install Iterm2
+
+os.system("brew install --cask iterm2")
+
+# Install Oh-My-ZSH
+
+os.system("sh -c '$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)'")
+
+# Install VS-CODE
+
+os.system("brew install --cask visual-studio-code")
+
+# Install VISUAL-STUDIO
+
+os.system("brew install --cask visual-studio")
+
+# Install BRAVE-BROWSER
+
+os.system("brew install --cask brave-browser")
+
+# Install UNITY
+
+os.system("brew install --cask unity")
+
+# Install Postman
+
+os.system("brew install --cask postman")
+
+# Install Arduino
+
+os.system("brew install --cask arduino")
+
+# Install Boop
+
+os.system("brew install --cask boop")
+
+# Install Terminus
+
+os.system("brew install --cask termius")
+
+# Install Pock
+
+os.system("brew install --cask pock")
+
+# Install Istat
+
+os.system("brew install --cask istat-menus")
+
+# Install Node
+
+os.system("brew install node")
+
+# Install Gitkraken
+
+os.system("brew install --cask gitkraken")
+
+# Install DotFiles
+
+import shutil
+import os
+    
+source_dir = './DotFiles'
+target_dir = '~/'
+    
+file_names = os.listdir(source_dir)
+    
+for file_name in file_names:
+    shutil.move(os.path.join(source_dir, file_name), target_dir)
