45f513468dc5d7eac4b624d96711ec25001a26b5
Max Campbell <41460735+maxall41@users.noreply.github.com>
2022-02-26 11:32:00 -0800

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -7,5 +7,7 @@ def bootstrap():
   os.system("npm install supports-hyperlinks")
 
 def pre_bootstrap():
+  # Make sure PIP is latest
+  os.system("pip3 install --upgrade pip3")
   # Install RICH
   os.system("pip3 install rich")
