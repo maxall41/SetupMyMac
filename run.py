8faf5356a10360cd06ba92dce856eddb8dbb7845
Max Campbell <41460735+maxall41@users.noreply.github.com>
2022-02-26 12:04:01 -0800

    Update run.py

--- run.py
+++ run.py
@@ -34,6 +34,10 @@ os.system("brew tap homebrew/cask")
 
 os.system("sh -c '$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)'")
 
+# Install Powerlevel10K
+
+os.system("git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k")
+
 # Install XXH
 
 os.system("brew install xxh")
