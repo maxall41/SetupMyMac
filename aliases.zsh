913846606c51b114340db20e77cfa6d549290c27
Max Campbell <41460735+maxall41@users.noreply.github.com>
2022-02-26 21:37:41 -0800

    Create aliases.zsh

---
+++ aliases.zsh
@@ -0,0 +1,23 @@
+alias nano='micro'
+alias ssh='xxh'
+alias c='clear'
+function up {
+       if [[ "$#" < 1 ]] ; then
+           cd ..
+   	   else
+           CDSTR=""
+           for i in {1..$1} ; do
+               CDSTR="../$CDSTR"
+           done
+           cd $CDSTR
+       fi
+}
+function cf {
+	pbcopy < $1
+	echo "Copied file contents"
+}
+alias g="git"
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ls='exa'
+alias cat='bat'
